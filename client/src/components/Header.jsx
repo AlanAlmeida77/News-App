e69d06dadfe48e3fa9124854041c8d6e1fae2bd6
Alan Almeida <alandavidalmeida@gmail.com>
2023-11-04 21:12:08 -0300

    changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,45 +1,11 @@
-import { useState } from 'react';
-import axios from 'axios';
+
+
 import styles from './header.module.css';
 
 const Header = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-
-  const handleSearch = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/api/news/search', {
-        params: {
-          query: searchTerm,
-        },
-      });
-
-      const newsData = response.data;
-      setSearchResults(newsData);
-    } catch (error) {
-      console.error('Error al buscar noticias:', error);
-    }
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
-  };
-
   return (
     <div className={styles.header}>
       <h1 className={styles.title}>Noticias</h1>
-      <div className={styles.searchContainer}>
-        <input
-          className={styles.placeholder}
-          type="text"
-          placeholder="Buscar noticias"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyPress={handleKeyPress}
-        <button onClick={handleSearch}>Buscar</button>
-      </div>
     </div>
   );
 };
